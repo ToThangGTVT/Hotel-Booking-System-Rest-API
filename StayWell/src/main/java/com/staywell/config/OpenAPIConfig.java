9a1a61fbc0a8123f7e8968adb9f7db2465688a01
ThangTQ <thang.to@golfzon.com>
2024-12-12 10:30:49 +0700

    change some config

--- StayWell/src/main/java/com/staywell/config/OpenAPIConfig.java
+++ StayWell/src/main/java/com/staywell/config/OpenAPIConfig.java
@@ -1,15 +1,38 @@
 package com.staywell.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class OpenAPIConfig {
+
     @Bean
     public OpenAPI customOpenAPI() {
+        Server prodServer = new Server()
+                .url("https://booking.setdanh.io.vn")
+                .description("Production Server");
+
+        Server localServer = new Server()
+                .url("http://localhost:8808")
+                .description("Local Development Server");
+
         return new OpenAPI()
+                .servers(List.of(prodServer, localServer))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .security(List.of(new SecurityRequirement().addList("bearerAuth")))
+
                 .info(new Info()
                         .title("Your API Title")
                         .version("1.0")
